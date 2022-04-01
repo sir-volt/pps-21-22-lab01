7c5140911f0ba4190c43994e39d5c89487969215
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-01 17:10:55 +0200

    made BankAccountATM.java a bit more readable

--- pps-lab01-intellij-basic-example/src/lab01/example/model/BankAccountATM.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/BankAccountATM.java
@@ -1,6 +1,6 @@
 package lab01.example.model;
 
-public class BankAccountATM extends AbstractbankAccount {
+public class BankAccountATM extends AbstractBankAccount {
 
     public BankAccountATM(final AccountHolder holder, final double balance) {
         super(holder,balance);
